5aea4f1c83b485ce6d6cde5694d169abbf7e3f1c
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-06-01 14:19:59 +0300

    Add as_tuple function

--- mytuple.py
+++ mytuple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+from typing import Any
 from operator import itemgetter
 
 
@@ -8,15 +9,12 @@ class TupleMeta(type):
     def __init__(cls, clsname, bases, clsdict):
         super().__init__(clsname, bases, clsdict)
         fields = clsdict.get("_fields", [])
-        if fields:
-            print(f"TupleMeta.__init__ {cls}")
         for i, name in enumerate(fields):
             setattr(cls, name, property(itemgetter(i)))
 
 
 class MyTuple(tuple, metaclass=TupleMeta):
     def __new__(cls, *args):
-        print(f"MyTuple.__new__ {cls}")
         if (n := len(cls._fields)) != len(args):
             raise TypeError(f"{cls.__name__} expects {n} arguments")
         return super().__new__(cls, args)
@@ -26,6 +24,10 @@ class Exercise(MyTuple):
     _fields = ["name", "weight", "reps"]
 
 
+def as_csv(t: MyTuple) -> tuple[Any, ...]:
+    return ", ".join((f"{f}={getattr(t, f)!r}" for f in t._fields))
+
+
 if __name__ == "__main__":
     exer = Exercise("bench press", 50.0, 4)
-    print(exer)
+    print(as_csv(exer))
