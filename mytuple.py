9f6c54911c2ed2be90bf305d8efb7e5a8bca8ef8
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-06-01 11:04:37 +0300

    Bugfix TupleMeta.__init__, MyTuple.__new__
    TupleMeta.__init__
    -        super().__init__(cls, clsname, bases, clsdict)
    . . .
    +        super().__init__(clsname, bases, clsdict)
    
    MyTuple.__new__
    -        return super().__new__(*args)
    +        return super().__new__(cls, args)

--- mytuple.py
+++ mytuple.py
@@ -6,18 +6,20 @@ from operator import itemgetter
 
 class TupleMeta(type):
     def __init__(cls, clsname, bases, clsdict):
-        print(f"__init__ {cls}")
-        super().__init__(cls, clsname, bases, clsdict)
-        for i, name in enumerate(clsdict.get("_fields", [])):
+        super().__init__(clsname, bases, clsdict)
+        fields = clsdict.get("_fields", [])
+        if fields:
+            print(f"TupleMeta.__init__ {cls}")
+        for i, name in enumerate(fields):
             setattr(cls, name, property(itemgetter(i)))
 
 
 class MyTuple(tuple, metaclass=TupleMeta):
     def __new__(cls, *args):
-        print(f"__new__ {cls}")
+        print(f"MyTuple.__new__ {cls}")
         if (n := len(cls._fields)) != len(args):
             raise TypeError(f"{cls.__name__} expects {n} arguments")
-        return super().__new__(*args)
+        return super().__new__(cls, args)
 
 
 class Exercise(MyTuple):
